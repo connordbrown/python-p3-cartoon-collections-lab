c709262c4d2d7fe51e95e7c98f059a3ce8c17d0a
Connor <connordbrown@gmail.com>
2023-11-02 20:32:52 -0600

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i in range(len(dwarves)):
+        print(f"{i+1}. {dwarves[i]}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_elements):
+    return [f'{element.title()}!' for element in planeteer_elements]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(ingredients):
+    for item in ingredients:
+        if item in ["cheddar", "gouda", "camembert"]:
+            return item
+    return None
